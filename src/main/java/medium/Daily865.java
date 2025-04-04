4ed853281b710c01243f2583e1196426118cd470
konanik15 <danielsopel14@gmail.com>
2025-04-04 22:14:21 +0200

    865 medium

---
+++ src/main/java/medium/Daily865.java
@@ -0,0 +1,26 @@
+package medium;
+
+import data_structures.TreeNode;
+
+public class Daily865 {
+
+    public TreeNode subtreeWithAllDeepest(TreeNode root) {
+        return dfs(root).node;
+    }
+
+    DepthOrientedTreeNode dfs(TreeNode node) {
+        if (node == null) return new DepthOrientedTreeNode(null, 0);
+
+        var left = dfs(node.left);
+        var right = dfs(node.right);
+
+        if (left.height == right.height) return new DepthOrientedTreeNode(node, 1 + left.height);
+        else if (left.height > right.height) return new DepthOrientedTreeNode(left.node, left.height + 1);
+        return new DepthOrientedTreeNode(right.node, right.height + 1);
+
+    }
+
+    record DepthOrientedTreeNode(TreeNode node, int height) {
+    }
+
+}
