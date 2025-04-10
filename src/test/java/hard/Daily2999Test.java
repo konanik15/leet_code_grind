d9bb981ffeb06711f14638fdefd4ec3626728fbb
konanik15 <danielsopel14@gmail.com>
2025-04-10 22:47:05 +0200

    2999 hard

---
+++ src/test/java/hard/Daily2999Test.java
@@ -0,0 +1,52 @@
+package hard;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class Daily2999Test {
+
+    Daily2999 daily2999;
+
+    @BeforeEach
+    void setUp() {
+        daily2999 = new Daily2999();
+    }
+
+    @Test
+    void numberOfPowerfulInt5() {
+        // given
+        var start = 1;
+        var finish = 6500;
+        var limit = 5;
+        var s = "124";
+
+        // when, then
+        assertEquals(5, daily2999.numberOfPowerfulInt(start, finish, limit, s));
+    }
+
+    @Test
+    void numberOfPowerfulInt2() {
+        // given
+        var start = 15;
+        var finish = 215;
+        var limit = 6;
+        var s = "1";
+
+        // when, then
+        assertEquals(2, daily2999.numberOfPowerfulInt(start, finish, limit, s));
+    }
+
+    @Test
+    void numberOfPowerfulInt0() {
+        // given
+        var start = 1000;
+        var finish = 2000;
+        var limit = 4;
+        var s = "3000";
+
+        // when, then
+        assertEquals(0, daily2999.numberOfPowerfulInt(start, finish, limit, s));
+    }
+}
